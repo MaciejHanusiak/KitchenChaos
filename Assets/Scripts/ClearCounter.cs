e07f703880870d2640d4423ba9b3377519862720
mhanusiak <maciej.hanusiak7@gmail.com>
2025-04-24 15:22:53 +0200

    Add logic for kitchen object to which clear counter kitchen object is attached and for clear counter to which kitchen object clear counter is attached

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -7,17 +7,56 @@ public class ClearCounter : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
     [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private ClearCounter secondClearCounter;
+    [SerializeField] private bool testing;
 
     private KitchenObject kitchenObject;
+
+    private void Update()
+    {
+        // testing Input key for pickup
+        if (testing && Input.GetKeyDown(KeyCode.T))
+        {
+            if (kitchenObject != null)
+            {
+                kitchenObject.SetClearCounter(secondClearCounter);
+                
+            }
+        }
+    }
     public void Interact()
     {
-        if (kitchenObject == null)
+        if (kitchenObject == null) 
         {
             Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint); // Create new kitchen object
-            kitchenObjectTransform.localPosition = Vector3.zero; // Set position of this object to zero
-            kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this); // Set this instance of ClerCounter to kitchen object
+
+        }
+        else
+        {
+            Debug.Log(kitchenObject.GetClearCounter());
         }
 
+    }
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return counterTopPoint;
+    }
+        public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+        public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
     }
 
 
--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -6,10 +6,43 @@ public class KitchenObject : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
+    private ClearCounter clearCounter;
     public KitchenObjectSO GetKitchenObjectSO()
     {
         return kitchenObjectSO;
 
+    }
+
+    public void SetClearCounter(ClearCounter clearCounter)
+    {
+        if (this.clearCounter != null)
+        {
+            // check if kitchen object has clear Counter, if yes set it to null
+            this.clearCounter.ClearKitchenObject();
+        }
+
+        // set clear counter of kitchen obj to new clear counter
+        this.clearCounter = clearCounter; 
+
+        if (clearCounter.HasKitchenObject())
+        {
+            
+            Debug.LogError("This Clear Counter already has a KitchenObject!");
+        }
+        else
+        {
+            // Set kitchen object to clear counter
+            clearCounter.SetKitchenObject(this);
+        }
+
+        // Set position of kitchen object in game
+        transform.parent = clearCounter.GetKitchenObjectFollowTransform();
+        transform.localPosition = Vector3.zero;
+        
+    }
+    public ClearCounter GetClearCounter()
+    {
+        return clearCounter;
     }
     
 }
