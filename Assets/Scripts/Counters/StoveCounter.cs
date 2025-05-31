c645427213eecb4d8b302db62c33101cf483dbf0
mhanusiak <maciej.hanusiak7@gmail.com>
2025-05-31 23:41:58 +0200

    Add pick up drop down logic to stove counter script

--- Assets/Scripts/Counters/StoveCounter.cs
+++ Assets/Scripts/Counters/StoveCounter.cs
@@ -1,18 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static CuttingCounter;
 
-public class StoveCounter : MonoBehaviour
+public class StoveCounter : BaseCounter
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private FryingRecipeSO[] fryingRecipeSOArray;
+
+    public override void Interact(Player player)
     {
-        
+        if (!HasKitchenObject())
+        {   // There is no kitchen object here
+
+            if (player.HasKitchenObject())
+            {
+                // Player is carrying something
+                if (HasRecipeWithInput(player.GetKitchenObject().GetKitchenObjectSO()))
+                {
+                    // Player has something that can be fried
+                    player.GetKitchenObject().SetKitchenObjectParent(this);
+                }
+
+            }
+            else
+            {
+                // Player not carrying anything
+            }
+        }
+        else
+        {   // There is kitchen object here
+
+            if (player.HasKitchenObject())
+            {
+                // Player has kitchen object
+            }
+            else
+            {
+                // The player does not have an object
+                this.GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private bool HasRecipeWithInput(KitchenObjectSO inputKitchenObjectSO)
     {
-        
+        FryingRecipeSO fryingRecipeSO = GetFryingRecipeSOWithInput(inputKitchenObjectSO);
+        return fryingRecipeSO != null;
+    }
+    private KitchenObjectSO GetOutputForInput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        FryingRecipeSO fryingRecipeSO = GetFryingRecipeSOWithInput(inputKitchenObjectSO);
+        if (fryingRecipeSO != null)
+        {
+            return fryingRecipeSO.output;
+        }
+        else
+        {
+            return null;
+        }
+    }
+    private FryingRecipeSO GetFryingRecipeSOWithInput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        foreach (FryingRecipeSO fryingRecipeSO in fryingRecipeSOArray)
+        {
+            if (fryingRecipeSO.input == inputKitchenObjectSO)
+            {
+                return fryingRecipeSO;
+            }
+        }
+        return null;
     }
 }
